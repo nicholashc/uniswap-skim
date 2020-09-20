20cf3ae4aee48b5e8452eb40bbd10e7b99237630
Nicholas <44581646+nicholashc@users.noreply.github.com>
2020-09-20 00:45:54 -0700

    Update Forwarder.sol
    fixed spelling in comment

--- Forwarder.sol
+++ Forwarder.sol
@@ -16,8 +16,8 @@ contract Forwarder {
     /// @dev Only the contract deployer is allowed to call this function
     /// @param add the address to forward to
     /// @param data arbitrary data of any length
-    /// @return success low level call faiiled or succeeded
-    /// @return message any data returned by the call (not necessarily anything)
+    /// @return success low level call failed or succeeded
+    /// @return message any data returned by the call (not necessarily anything will be returned even on success)
     function forward(address payable add, bytes calldata data) external payable returns(bool success, bytes memory message) {
         require(msg.sender == owner);
         return add.call{value: msg.value}(data);
